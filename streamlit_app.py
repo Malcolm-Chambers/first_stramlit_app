27d838db5d1697e6eaa9601046220a760f8586db
Malcolm-Chambers <malcolm.chambers@live.com>
2022-06-10 09:15:37 +1200

    Update streamlit_app.py
    add emoji to soomthies

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kasle, Spinach & rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
-streamlit.header('Build Your Own Fruit Smoothie')
+streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
