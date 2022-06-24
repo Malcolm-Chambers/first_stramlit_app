cee3520e64764e673017a7927299b4fd5000803e
Malcolm-Chambers <malcolm.chambers@live.com>
2022-06-24 15:44:42 +1200

    add user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,11 @@ fruit_to_Show = my_fruit_list.loc[fruitSelected]
 
 streamlit.dataframe(fruit_to_Show)
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
