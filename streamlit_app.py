c3d5ecb31e764e70a862f640eaa413946823f4b6
Malcolm-Chambers <malcolm.chambers@live.com>
2022-06-24 15:28:49 +1200

    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,5 @@ fruit_to_Show = my_fruit_list.loc[fruitSelected]
 streamlit.dataframe(fruit_to_Show)
 
 import requests
-fruityvice_response = requests.get("https://fruitvice.com/api/fruit/watermelon")
-streamlit.text(fruitvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
